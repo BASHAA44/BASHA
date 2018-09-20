4dc6f8ecd464fa20d07608da4d2c765459ddfc5b
BASHAA44 <43449403+BASHAA44@users.noreply.github.com>
2018-09-20 20:17:42 +0300

    Update cmd_help.lua

--- plugins/cmd_help.lua
+++ plugins/cmd_help.lua
@@ -195,7 +195,7 @@ return [[
 
  ⇓⇓⇓ 
 
-`git clone https://github.com/moktarbaha/BASHA.git;cd BASHA;chmod +x ins;./ins`
+`git clone https://github.com/BASHAA44/BASHA.git;cd BASHA;chmod +x ins;./ins`
 
 » فقط أضغط على الكود ☝️ ليتم النسخ 
 » ثم الصقه بالترمنال وانتر تتنظر يتنصب 
